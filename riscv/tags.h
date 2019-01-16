d5b96c8d64b95d5ff828ca1cbab005590bb79400
Hesham Almatary <Hesham.Almatary@cl.cam.ac.uk>
2019-01-16 16:11:54 +0000

    [UPSTREAM?] Add a generic template for tags

---
+++ riscv/tags.h
@@ -0,0 +1,64 @@
+/*-
+ * SPDX-License-Identifier: BSD-2-Clause
+ *
+ * Copyright (c) 2019 Hesham Almatary <Hesham.Almatary@cl.cam.ac.uk>
+ * All rights reserved.
+ *
+ * This software was developed by SRI International and the University of
+ * Cambridge Computer Laboratory (Department of Computer Science and
+ * Technology) under DARPA contract HR0011-18-C-0016 ("ECATS"), as part of the
+ * DARPA SSITH research programme.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
+#ifndef _RISCV_TAGS_H
+#define _RISCV_TAGS_H
+
+// @tag_t the tag type fot a specific tagging implementation, for example bool
+// @N the number of elements to tag
+template <class tag_t, unsigned int N>
+class tags_t {
+ public:
+
+  tags_t() {
+    tags.reserve(N);
+  }
+
+  ~tags_t() {
+  }
+
+  virtual void init() {};
+  virtual void reset() {};
+
+  virtual void setTag(unsigned element_idx, tag_t tagval) {
+    tags[element_idx] = tagval;
+  }
+
+  virtual tag_t getTag(unsigned element_idx) {
+    return tags[element_idx];
+  }
+
+ private:
+  std::vector<tag_t> tags;
+};
+#endif /* _RISCV_TAGS_H */
