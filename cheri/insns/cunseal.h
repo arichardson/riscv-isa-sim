154d139219cc9de16714bcde432e52b508d4351c
Alexandre Joannou <aj443@cam.ac.uk>
2019-02-27 15:22:44 +0000

    CHERI: Extend cunseal with exceptions

--- cheri/insns/cunseal.h
+++ cheri/insns/cunseal.h
@@ -1,9 +1,18 @@
 // See LICENSE_CHERI for license details.
 
-cheri_reg_t temp = CS1;
-temp.sealed = 0;
-temp.otype = 0;
-/* FIXME: cs.perms.Global and ct.perms.Global */
-temp.perms = CS1.perms & CS2.perms;
-WRITE_CD(temp);
-
+if (!CS1.tag) CHERI->raise_trap(CAUSE_CHERI_TAG_FAULT, insn.cs1());
+else if (!CS2.tag) CHERI->raise_trap(CAUSE_CHERI_TAG_FAULT, insn.cs2());
+else if (!CS1.sealed) CHERI->raise_trap(CAUSE_CHERI_SEAL_FAULT, insn.cs1());
+else if (CS2.sealed) CHERI->raise_trap(CAUSE_CHERI_SEAL_FAULT, insn.cs2());
+else if (CS2.base + CS2.offset != CS1.otype) CHERI->raise_trap(CAUSE_CHERI_TYPE_FAULT, insn.cs2());
+/* FIXME */
+//else if (!CS2.perms.permitUnseal) CHERI->raise_trap(CAUSE_CHERI_PERMIT_UNSEAL_FAULT, insn.cs2());
+else if (CS2.base + CS2.offset >= CS2.base + CS2.length) CHERI->raise_trap(CAUSE_CHERI_LENGTH_FAULT, insn.cs2());
+else {
+  cheri_reg_t temp = CS1;
+  temp.sealed = 0;
+  temp.otype = 0;
+  /* FIXME: cs.perms.Global and ct.perms.Global */
+  temp.perms = CS1.perms & CS2.perms;
+  WRITE_CD(temp);
+}
