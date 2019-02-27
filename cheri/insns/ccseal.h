ae44f354af3c20df70365f0f8bbbd0db11dd5dc2
Alexandre Joannou <aj443@cam.ac.uk>
2019-02-27 19:48:39 +0000

    CHERI: Mockup implementation of ccseal

--- cheri/insns/ccseal.h
+++ cheri/insns/ccseal.h
@@ -1,3 +1,19 @@
 // See LICENSE_CHERI for license details.
 
-// TODO
+if (!CS1.tag) CHERI->raise_trap(CAUSE_CHERI_TAG_FAULT, insn.cs1());
+else if (!CS2.tag || CS2.base + CS2.offset == UINT64_MAX) WRITE_CD(CS1);
+else if (CS1.sealed) CHERI->raise_trap(CAUSE_CHERI_SEAL_FAULT, insn.cs1());
+else if (CS2.sealed) CHERI->raise_trap(CAUSE_CHERI_SEAL_FAULT, insn.cs2());
+/* FIXME */
+//else if (!CS2.perms.permitSeal) CHERI->raise_trap(CAUSE_CHERI_PERMIT_SEAL_FAULT, insn.cs2());
+else if (CS2.base + CS2.offset >= CS2.base + CS2.length) CHERI->raise_trap(CAUSE_CHERI_LENGTH_FAULT, insn.cs2());
+/* FIXME */
+//else if (CS2.base + CS2.offset > otypeMAX) CHERI->raise_trap(CAUSE_CHERI_LENGTH_FAULT, insn.cs2());
+/* FIXME */
+// else if (!new_cap.exact) CHERI->raise_trap(CAUSE_CHERI_BOUNDS_FAULT, insn.cs1());
+else {
+  cheri_reg_t temp = CS1;
+  temp.sealed = 1;
+  temp.otype = CS2.base + CS2.offset;
+  WRITE_CD(temp);
+}
