d9b4c7b075e8167641b56909e043339a1843663d
Alexandre Joannou <aj443@cam.ac.uk>
2019-02-27 15:20:10 +0000

    CHERI: Extend csetboundsexact with exceptions

--- cheri/insns/csetboundsexact.h
+++ cheri/insns/csetboundsexact.h
@@ -1,7 +1,14 @@
 // See LICENSE_CHERI for license details.
 
-cheri_reg_t temp = CS1;
-temp.base = CS1.base + CS1.offset;
-temp.length = RS2;
-temp.offset = 0;
-WRITE_CD(temp);
+if (!CS1.tag) CHERI->raise_trap(CAUSE_CHERI_TAG_FAULT, insn.cs1());
+else if (CS1.sealed) CHERI->raise_trap(CAUSE_CHERI_SEAL_FAULT, insn.cs1());
+else if (CS1.base + CS1.offset + RS2 > CS1.base + CS1.length) CHERI->raise_trap(CAUSE_CHERI_LENGTH_FAULT, insn.cs1());
+/* FIXME */
+//else if (!new_cap.exact) CHERI->raise_trap(CAUSE_CHERI_BOUNDS_FAULT, insn.cs1());
+else {
+  cheri_reg_t temp = CS1;
+  temp.base = CS1.base + CS1.offset;
+  temp.length = RS2;
+  temp.offset = 0;
+  WRITE_CD(temp);
+}
