0ebf235281221d1b580b84c1e37f842880941a36
Alexandre Joannou <aj443@cam.ac.uk>
2019-02-27 19:47:08 +0000

    CHERI: Extend candperm with exceptions

--- cheri/insns/candperm.h
+++ cheri/insns/candperm.h
@@ -1,6 +1,10 @@
 // See LICENSE_CHERI for license details.
 
-cheri_reg_t temp = CS1;
-temp.perms = CS1.perms & (RS2 & MASK(CHERI_PERM_BITS));
-temp.uperms = CS1.uperms & ((RS2 >> CHERI_PERM_BITS) & MASK(CHERI_USER_PERM_BITS));
-WRITE_CD(temp);
+if (!CS1.tag) CHERI->raise_trap(CAUSE_CHERI_TAG_FAULT, insn.cs1());
+else if (CS1.sealed) CHERI->raise_trap(CAUSE_CHERI_SEAL_FAULT, insn.cs1());
+else {
+  cheri_reg_t temp = CS1;
+  temp.perms = CS1.perms & (RS2 & MASK(CHERI_PERM_BITS));
+  temp.uperms = CS1.uperms & ((RS2 >> CHERI_PERM_BITS) & MASK(CHERI_USER_PERM_BITS));
+  WRITE_CD(temp);
+}
