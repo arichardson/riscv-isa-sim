1b4d38475b70a803cb2db3b8d20dd5e06b2af656
Alexandre Joannou <aj443@cam.ac.uk>
2019-02-27 19:49:51 +0000

    CHERI: Extend csetoffset with exceptions

--- cheri/insns/csetoffset.h
+++ cheri/insns/csetoffset.h
@@ -1,5 +1,10 @@
 // See LICENSE_CHERI for license details.
 
-cheri_reg_t temp = CS1;
-temp.offset = RS2;
-WRITE_CD(temp);
+if (CS1.tag && CS1.sealed) CHERI->raise_trap(CAUSE_CHERI_SEAL_FAULT, insn.cs1());
+/* FIXME */
+// else if (not exact set offset) WRITE_CD(null_with_addr(CS1.base + RS2));
+else {
+  cheri_reg_t temp = CS1;
+  temp.offset = RS2;
+  WRITE_CD(temp);
+}
