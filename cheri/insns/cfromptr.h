528c494372c9193564a2ae4c84bc18262f1cdb35
Alexandre Joannou <aj443@cam.ac.uk>
2019-02-27 19:48:57 +0000

    CHERI: Extend cfromptr with exceptions

--- cheri/insns/cfromptr.h
+++ cheri/insns/cfromptr.h
@@ -1,5 +1,12 @@
 // See LICENSE_CHERI for license details.
 
-cheri_reg_t temp = CS1;
-temp.offset = RS2;
-WRITE_CD(temp);
+if (RS2 == 0) WRITE_CD(null_cap);
+else if (!CS1.tag) CHERI->raise_trap(CAUSE_CHERI_TAG_FAULT, insn.cs1());
+else if (CS1.sealed) CHERI->raise_trap(CAUSE_CHERI_SEAL_FAULT, insn.cs1());
+/* FIXME */
+// else if (!newcap.exact) WRITE_CD(nullWithAddr(CS1.base + RS2));
+else {
+  cheri_reg_t temp = CS1;
+  temp.offset = RS2;
+  WRITE_CD(temp);
+}
