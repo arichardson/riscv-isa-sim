d93e5831485420f6c5f5bbd48cc252a462564b07
Alexandre Joannou <aj443@cam.ac.uk>
2019-02-27 19:50:43 +0000

    CHERI: Extend csetboundsimmediate with exceptions

--- cheri/insns/csetboundsimmediate.h
+++ cheri/insns/csetboundsimmediate.h
@@ -1,8 +1,13 @@
 // See LICENSE_CHERI for license details.
 
-cheri_reg_t temp = CS1;
-temp.base = CS1.offset;
-temp.length = insn.i_imm();
-temp.offset = 0;
-WRITE_CD(temp);
-
+if (!CS1.tag) CHERI->raise_trap(CAUSE_CHERI_TAG_FAULT, insn.cs1());
+else if (CS1.sealed) CHERI->raise_trap(CAUSE_CHERI_SEAL_FAULT, insn.cs1());
+else if (CS1.base + CS1.offset < CS1.base) CHERI->raise_trap(CAUSE_CHERI_LENGTH_FAULT, insn.cs1());
+else if (CS1.base + CS1.offset + insn.i_imm() > CS1.base + CS1.length) CHERI->raise_trap(CAUSE_CHERI_LENGTH_FAULT, insn.cs1());
+else {
+  cheri_reg_t temp = CS1;
+  temp.base = CS1.base + CS1.offset;
+  temp.length = insn.i_imm();
+  temp.offset = 0;
+  WRITE_CD(temp);
+}
