08b21013509b83c5042792b69d48b0fc1fb4f6a4
Hesham Almatary <Hesham.Almatary@cl.cam.ac.uk>
2019-05-21 15:43:39 +0100

    CHERI: Implement csetflags/cgetflags instructions

---
+++ cheri/insns/csetflags.h
@@ -0,0 +1,10 @@
+// See LICENSE_CHERI for license details.
+
+if (CS1.tag && CS1.sealed) {
+  CHERI->raise_trap(CAUSE_CHERI_SEAL_FAULT, insn.cs1());
+}
+else {
+  cheri_reg_t temp = CS1;
+  temp.flags = RS2;
+  WRITE_CD(temp);
+}
