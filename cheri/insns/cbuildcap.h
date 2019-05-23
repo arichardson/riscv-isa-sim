a1db6bec47fc4e0c1e0a5c1d71e8f8926fccd5b6
Hesham Almatary <Hesham.Almatary@cl.cam.ac.uk>
2019-05-23 13:31:36 +0100

    CHERI: Trivial - Fix a compiler warning and re-format

--- cheri/insns/cbuildcap.h
+++ cheri/insns/cbuildcap.h
@@ -1,15 +1,21 @@
 // See LICENSE_CHERI for license details.
 
-if (!CS1.tag) CHERI->raise_trap(CAUSE_CHERI_TAG_FAULT, insn.cs1());
-else if (CS1.sealed) CHERI->raise_trap(CAUSE_CHERI_SEAL_FAULT, insn.cs1());
-else if (CS2.base < CS1.base) CHERI->raise_trap(CAUSE_CHERI_LENGTH_FAULT, insn.cs1());
-else if (CS2.base + CS2.length > CS1.base + CS1.length) CHERI->raise_trap(CAUSE_CHERI_LENGTH_FAULT, insn.cs1());
-else if (CS2.length < 0) CHERI->raise_trap(CAUSE_CHERI_LENGTH_FAULT, insn.cs2());
-else if (CS1.perms & CS2.perms != CS2.perms) CHERI->raise_trap(CAUSE_CHERI_ACCESSPERM_FAULT, insn.cs1());
+if (!CS1.tag)
+  CHERI->raise_trap(CAUSE_CHERI_TAG_FAULT, insn.cs1());
+else if (CS1.sealed)
+  CHERI->raise_trap(CAUSE_CHERI_SEAL_FAULT, insn.cs1());
+else if (CS2.base < CS1.base)
+  CHERI->raise_trap(CAUSE_CHERI_LENGTH_FAULT, insn.cs1());
+else if (CS2.base + CS2.length > CS1.base + CS1.length)
+  CHERI->raise_trap(CAUSE_CHERI_LENGTH_FAULT, insn.cs1());
+else if (CS2.length < 0)
+  CHERI->raise_trap(CAUSE_CHERI_LENGTH_FAULT, insn.cs2());
+else if ((CS1.perms & CS2.perms) != CS2.perms)
+  CHERI->raise_trap(CAUSE_CHERI_ACCESSPERM_FAULT, insn.cs1());
 else {
   cheri_reg_t temp = CS2;
   temp.sealed = 0;
-  temp.otype = 0xffffff;
+  temp.otype = OTYPE_UNSEALED;
   temp.tag = 1;
   WRITE_CD(temp);
 }
