b9ed29ae64e788d2fa5def49124800f305103545
Hesham Almatary <Hesham.Almatary@cl.cam.ac.uk>
2019-05-23 11:28:25 +0100

    CHERI: Update/fix cfromptr with latest spec

--- cheri/insns/cfromptr.h
+++ cheri/insns/cfromptr.h
@@ -1,12 +1,19 @@
 // See LICENSE_CHERI for license details.
 
-if (RS2 == 0) WRITE_CD(CHERI_NULL_CAP);
-else if (!CS1.tag) CHERI->raise_trap(CAUSE_CHERI_TAG_FAULT, insn.cs1());
-else if (CS1.sealed) CHERI->raise_trap(CAUSE_CHERI_SEAL_FAULT, insn.cs1());
+cheri_reg_t tmp = (insn.cs1() == 0)? DDC : CS1;
+
+if (RS2 == 0) {
+  WRITE_CD(CHERI_NULL_CAP);
+} else if (!tmp.tag) {
+  CHERI->raise_trap(CAUSE_CHERI_TAG_FAULT, insn.cs1());
+}
+else if (tmp.otype != OTYPE_UNSEALED) {
+  CHERI->raise_trap(CAUSE_CHERI_SEAL_FAULT, insn.cs1());
+}
 /* FIXME */
 // else if (!newcap.exact) WRITE_CD(nullWithAddr(CS1.base + RS2));
 else {
-  cheri_reg_t temp = CS1;
+  cheri_reg_t temp = tmp;
   temp.offset = RS2;
   WRITE_CD(temp);
 }
