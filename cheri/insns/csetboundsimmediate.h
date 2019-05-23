e1041e9676c57645eb02ecb69237f1b640ce883d
Hesham Almatary <Hesham.Almatary@cl.cam.ac.uk>
2019-05-23 11:57:25 +0100

    CHERI: Update/fix csetboundsimm with latest spec
    Use unsigned immediate

--- cheri/insns/csetboundsimmediate.h
+++ cheri/insns/csetboundsimmediate.h
@@ -1,9 +1,18 @@
 // See LICENSE_CHERI for license details.
 
-if (!CS1.tag) CHERI->raise_trap(CAUSE_CHERI_TAG_FAULT, insn.cs1());
-else if (CS1.sealed) CHERI->raise_trap(CAUSE_CHERI_SEAL_FAULT, insn.cs1());
-else if (CS1.base + CS1.offset < CS1.base) CHERI->raise_trap(CAUSE_CHERI_LENGTH_FAULT, insn.cs1());
-else if (CS1.base + CS1.offset + insn.i_imm() > CS1.base + CS1.length) CHERI->raise_trap(CAUSE_CHERI_LENGTH_FAULT, insn.cs1());
+if (!CS1.tag) {
+  CHERI->raise_trap(CAUSE_CHERI_TAG_FAULT, insn.cs1());
+}
+else if (CS1.otype != OTYPE_UNSEALED) {
+  CHERI->raise_trap(CAUSE_CHERI_SEAL_FAULT,
+  insn.cs1());
+}
+else if (CS1.base + CS1.offset < CS1.base) {
+  CHERI->raise_trap(CAUSE_CHERI_LENGTH_FAULT, insn.cs1());
+}
+else if (CS1.base + CS1.offset + (insn.u_imm()) > CS1.base + CS1.length) {
+  CHERI->raise_trap(CAUSE_CHERI_LENGTH_FAULT, insn.cs1());
+}
 else {
   cheri_reg_t temp = CS1;
   temp.base = CS1.base + CS1.offset;
