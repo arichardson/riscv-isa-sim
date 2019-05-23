3c58738783718658890805ddcfe6345a716a09e6
Hesham Almatary <Hesham.Almatary@cl.cam.ac.uk>
2019-05-23 11:18:20 +0100

    CHERI: Fix/update csetboundsexact to use OTYPE_UNSEALED

--- cheri/insns/csetboundsexact.h
+++ cheri/insns/csetboundsexact.h
@@ -1,8 +1,19 @@
 // See LICENSE_CHERI for license details.
 
-if (!CS1.tag) CHERI->raise_trap(CAUSE_CHERI_TAG_FAULT, insn.cs1());
-else if (CS1.sealed) CHERI->raise_trap(CAUSE_CHERI_SEAL_FAULT, insn.cs1());
-else if (CS1.base + CS1.offset + RS2 > CS1.base + CS1.length) CHERI->raise_trap(CAUSE_CHERI_LENGTH_FAULT, insn.cs1());
+reg_t cursor = CS1.base + CS1.offset;
+
+if (!CS1.tag) {
+  CHERI->raise_trap(CAUSE_CHERI_TAG_FAULT, insn.cs1());
+}
+else if (CS1.otype != OTYPE_UNSEALED) {
+  CHERI->raise_trap(CAUSE_CHERI_SEAL_FAULT, insn.cs1());
+}
+else if (cursor < CS1.base) {
+  CHERI->raise_trap(CAUSE_CHERI_LENGTH_FAULT, insn.cs1());
+}
+else if (cursor + RS2 > CS1.base + CS1.length) {
+  CHERI->raise_trap(CAUSE_CHERI_LENGTH_FAULT, insn.cs1());
+}
 /* FIXME */
 //else if (!new_cap.exact) CHERI->raise_trap(CAUSE_CHERI_BOUNDS_FAULT, insn.cs1());
 else {
