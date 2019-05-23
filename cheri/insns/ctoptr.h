0d147a60ea2fba3f1d52907f1c510ae659bd89b9
Hesham Almatary <Hesham.Almatary@cl.cam.ac.uk>
2019-05-23 11:14:48 +0100

    CHERI: Update/fix ctoptr with latest spec

--- cheri/insns/ctoptr.h
+++ cheri/insns/ctoptr.h
@@ -1,6 +1,12 @@
 // See LICENSE_CHERI for license details.
 
-if (!CS2.tag) CHERI->raise_trap(CAUSE_CHERI_TAG_FAULT, insn.cs2());
-else if (!CS1.tag) WRITE_RD(0);
-else if (CS1.sealed) CHERI->raise_trap(CAUSE_CHERI_SEAL_FAULT, insn.cs1());
-else WRITE_RD(CS1.base + CS1.offset - CS2.base);
+cheri_reg_t tmp = (insn.cs2() == 0)? DDC : CS2;
+
+if (!tmp.tag) {
+  CHERI->raise_trap(CAUSE_CHERI_TAG_FAULT, insn.cs2());
+}
+else if (CS1.otype != OTYPE_UNSEALED && CS1.tag) {
+  CHERI->raise_trap(CAUSE_CHERI_SEAL_FAULT, insn.cs1());
+} else {
+  WRITE_RD(CS1.tag? CS1.base + CS1.offset - tmp.base : 0);
+}
