a0fdacedee4cfe838bab839cdd8814ce573051a9
Marno <Marno.van-der-Maas@cl.cam.ac.uk>
2019-07-18 16:19:18 +0100

    Fixed addition overflow bug in csetbounds.

--- cheri/insns/csetbounds.h
+++ cheri/insns/csetbounds.h
@@ -3,6 +3,7 @@
 if (!CS1.tag) CHERI->raise_trap(CAUSE_CHERI_TAG_FAULT, insn.cs1());
 else if (CS1.sealed) CHERI->raise_trap(CAUSE_CHERI_SEAL_FAULT, insn.cs1());
 else if (CS1.base + CS1.offset < CS1.base) CHERI->raise_trap(CAUSE_CHERI_LENGTH_FAULT, insn.cs1());
+else if (CS1.base + CS1.offset + RS2 < CS1.base + CS1.offset) CHERI->raise_trap(CAUSE_CHERI_LENGTH_FAULT, insn.cs1()); //Addition overflow detection
 else if (CS1.base + CS1.offset + RS2 > CS1.base + CS1.length) CHERI->raise_trap(CAUSE_CHERI_LENGTH_FAULT, insn.cs1());
 else {
   cheri_reg_t temp = CS1;
