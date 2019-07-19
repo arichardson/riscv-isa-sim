bedda971f2791348114d36521b222cabc6033ee4
Marno <Marno.van-der-Maas@cl.cam.ac.uk>
2019-07-19 12:43:39 +0100

    Fixed a bug where csetoffset did not check for representability when setting offset.

--- cheri/insns/csetoffset.h
+++ cheri/insns/csetoffset.h
@@ -1,10 +1,15 @@
 // See LICENSE_CHERI for license details.
 
+uint64_t new_offset = RS2;
+
 if (CS1.tag && CS1.sealed) CHERI->raise_trap(CAUSE_CHERI_SEAL_FAULT, insn.cs1());
-/* FIXME */
-// else if (not exact set offset) WRITE_CD(null_with_addr(CS1.base + RS2));
+else if (!cc128_is_representable(CS1.sealed, CS1.base, CS1.length, new_offset, new_offset)) {
+  cheri_reg_t temp = CHERI_NULL_CAP;
+  temp.offset = new_offset; //Set temp.address = cb_val.base + rt_val
+  WRITE_CD(temp);
+}
 else {
   cheri_reg_t temp = CS1;
-  temp.offset = RS2;
+  temp.offset = new_offset;
   WRITE_CD(temp);
 }
