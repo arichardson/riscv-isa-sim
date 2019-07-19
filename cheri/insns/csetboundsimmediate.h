b2829c64c11c40f080e5dbe3ffce8805ad9b480d
Marno <Marno.van-der-Maas@cl.cam.ac.uk>
2019-07-19 13:42:38 +0100

    Made better changes to csetboundsimmediate.h that also uses the unsigned length for the exception checks.

--- cheri/insns/csetboundsimmediate.h
+++ cheri/insns/csetboundsimmediate.h
@@ -2,6 +2,8 @@
 
 //fprintf(stderr, "CHERI: set bounds immediate  base 0x%016lx offset 0x%016lx immdeiate 0x%lx length 0x%016lx.\n", CS1.base, CS1.offset, insn.i_imm(), CS1.length);
 
+uint64_t new_length = insn.i_imm() & 0x0FFF; // remove the sign extension from i_imm
+
 if (!CS1.tag) {
   CHERI->raise_trap(CAUSE_CHERI_TAG_FAULT, insn.cs1());
 }
@@ -11,16 +13,16 @@ else if (CS1.sealed) {
 else if (CS1.base + CS1.offset < CS1.base) {
   CHERI->raise_trap(CAUSE_CHERI_LENGTH_FAULT, insn.cs1());
 }
-else if (CS1.base + CS1.offset + (insn.i_imm()) < CS1.base + CS1.offset) { //check if the base+offset+immediate overflows or not
+else if (CS1.base + CS1.offset + new_length < CS1.base + CS1.offset) { //check if the base+offset+immediate overflows or not
   CHERI->raise_trap(CAUSE_CHERI_LENGTH_FAULT, insn.cs1());
 }
-else if (CS1.base + CS1.offset + (insn.i_imm()) > CS1.base + CS1.length) {
+else if (CS1.base + CS1.offset + new_length > CS1.base + CS1.length) {
   CHERI->raise_trap(CAUSE_CHERI_LENGTH_FAULT, insn.cs1());
 }
 else {
   cheri_reg_t temp = CS1;
   temp.base = CS1.base + CS1.offset;
-  temp.length = insn.i_imm() & 0x0FFF; //remove the sign extension form i_imm
+  temp.length = new_length;
   temp.offset = 0;
   WRITE_CD(temp);
 }
