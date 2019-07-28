bbbd72f547dfcb6147f63512bcb50005f6b4ebcb
Hesham Almatary <Hesham.Almatary@cl.cam.ac.uk>
2019-07-28 18:05:32 +0100

    CHERI: clc - Add a TODO and initial support for merged-rf

--- cheri/insns/clc.h
+++ cheri/insns/clc.h
@@ -6,14 +6,25 @@ reg_t paddr = CHERI->get_mmu()->translate(addr, 1, LOAD);
 
 cheri_reg_t cd = CHERI->get_mmu()->load_cheri_reg(paddr);
 
+// TODO: Fully implement this instruction to match the spec
+
 if (!CHERI->cheriMem_getTag(paddr)) {
 #if DEBUG
   printf("CHERI: Memory tag is not set\n");
 #endif
-
-  CHERI->raise_trap(CAUSE_CHERI_TAG_FAULT, insn.cs2());
+  cd.tag = 0;
+#ifdef CHERI_MERGED_RF
+  WRITE_CD_MERGED(cd);
+#else
+  WRITE_CD(cd);
+#endif /* CHERI_MERGED_RF */
+  //CHERI->raise_trap(CAUSE_CHERI_TAG_FAULT, insn.cs2());
 } else {
   cd.tag = 1;
+#ifdef CHERI_MERGED_RF
+  WRITE_CD_MERGED(cd);
+#else
   WRITE_CD(cd);
+#endif
 }
 
