c48cef62243ffcfa08dae7bcf99afcbeb9f700fc
Marno <Marno.van-der-Maas@cl.cam.ac.uk>
2019-07-29 18:45:02 +0100

    Fixed wrong offset being written on being unrepresentable in cincoffset.

--- cheri/insns/cincoffset.h
+++ cheri/insns/cincoffset.h
@@ -6,14 +6,11 @@ uint64_t new_offset = CS1.offset + RS2;
 
 if (CS1.tag && CS1.sealed) CHERI->raise_trap(CAUSE_CHERI_SEAL_FAULT, insn.cs1());
 else if (!cheri_is_representable(CS1.sealed, CS1.base, CS1.length, new_offset)) {
-  //fprintf(stderr, "not representable\n");
   cheri_reg_t temp = CHERI_NULL_CAP;
-  temp.base = CS1.base;
-  temp.offset = new_offset; //Set temp.address = cb_val.address + rt_val
+  temp.offset = CS1.base + new_offset; //Set temp.address = cb_val.address + rt_val
   WRITE_CD(temp);
 }
 else {
-  //fprintf(stderr, "representable\n");
   cheri_reg_t temp = CS1;
   temp.offset = new_offset;
   WRITE_CD(temp);
