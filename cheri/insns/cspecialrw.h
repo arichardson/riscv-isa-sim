24dc6eabc157dfd3400043d524ca8a3640f18c10
Marno <Marno.van-der-Maas@cl.cam.ac.uk>
2019-08-01 14:19:34 +0100

    Raising exception when trying to read or write to a special capability register that does not exist.

--- cheri/insns/cspecialrw.h
+++ cheri/insns/cspecialrw.h
@@ -1,14 +1,30 @@
 // See LICENSE_CHERI for license details.
 
-cheri_reg_t csr = CHERI_STATE.csrs_reg_file[insn.chs()];
-cheri_reg_t cs1 = CS1;
-
 cheri_reg_t temp = CSR;
 
-/* If source register is c0, don't write CSR (used to read CSR) */
-if (insn.cs1())
-  CSR = CS1;
+switch(insn.chs()) {
+  case CHERI_CSR_PCC:
+  case CHERI_CSR_DDC:
+  case CHERI_CSR_UTCC:
+  case CHERI_CSR_USCRATCHC:
+  case CHERI_CSR_UEPCC:
+  case CHERI_CSR_STCC:
+  case CHERI_CSR_SSCRATCHC:
+  case CHERI_CSR_SEPCC:
+  case CHERI_CSR_MTCC:
+  case CHERI_CSR_MSCRATCHC:
+  case CHERI_CSR_MEPCC:
 
-/* If destination register is c0, do nothing */
-if (insn.cd())
-  WRITE_CD(temp);
+    /* If source register is c0, don't write CSR (used to read CSR) */
+    if (insn.cs1())
+      CSR = CS1;
+
+    /* If destination register is c0, do nothing */
+    if (insn.cd())
+      WRITE_CD(temp);
+    break;
+
+  default:
+    throw trap_illegal_instruction(0);
+    break;
+}
