356a80fcc0027def30bf6cbf85660388da7f9338
Marno <Marno.van-der-Maas@cl.cam.ac.uk>
2019-09-18 12:09:48 +0100

    Implemented ctestsubset.

--- cheri/insns/ctestsubset.h
+++ cheri/insns/ctestsubset.h
@@ -1,3 +1,14 @@
 // See LICENSE_CHERI for license details.
 
-// TODO
+cheri_reg_t tmp = (insn.cs1() == 0)? DDC : CS1;
+
+if (
+  tmp.tag != CS2.tag ||
+  CS2.base < tmp.base ||
+  CS2.length > tmp.length ||
+  (CS2.perms & tmp.perms) != CS2.perms
+) {
+  WRITE_RD(0);
+} else {
+  WRITE_RD(1);
+}
